4cee2396df5f08db2878c9c1e703cf256ac9107e
Richard Westmoreland <kegtrack@gmail.com>
2020-03-15 11:52:22 -0500

    Add flight routes

--- routes/flights.js
+++ routes/flights.js
@@ -1,4 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const {
+	getFlights,
+	addFlight,
+	deleteFlight
+} = require('../controllers/flights');
 
-module.exports = router
+router
+	.route('/')
+	.get(getFlights)
+	.post(addFlight);
+
+router.route('/:id').delete(deleteFlight);
+
+module.exports = router;
