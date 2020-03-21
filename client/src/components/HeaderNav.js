58d4c73bee6a0473b6fdbb9aa62465764788d3b1
Richard Westmoreland <kegtrack@gmail.com>
2020-03-21 08:58:57 -0500

    Begin navbar work

--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -1,46 +1,76 @@
-import React, { useState } from 'react'
-import navStyles from '../styles/NavStyles'
-import AppBar from '@material-ui/core/AppBar'
-import ToolBar from '@material-ui/core/ToolBar'
-import IconButton from '@material-ui/core/IconButton'
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import MenuIcon from '@material-ui/icons/Menu'
-import Typography from '@material-ui/core/Typography'
+import React, { useState, useEffect } from 'react';
+import navStyles from '../styles/NavStyles';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
 
 const HeaderNav = () => {
-	const classes = navStyles()
-	const [auth, setAuth] = useState(true)
-	const [anchorEl, setAnchorEl] = useState(null)
-	const open = Boolean(anchorEl)
+	const classes = navStyles();
+	const [auth, setAuth] = useState(true);
+	const [isDesktop, setIsDesktop] = useState(false);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+
+	useEffect(() => {
+		window.addEventListener('resize', console.log('butts'));
+	}, []);
 
 	const handleMenu = () => {
-		console.log("butts")
-	}
+		console.log('butts');
+	};
+
+	const toggle = () => {
+		console.log('login Modal');
+	};
 
 	return (
 		<div className={classes.root}>
-		<AppBar position='static'>
-			<ToolBar variant="dense" className={classes.menuBar}>
-				<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-					<MenuIcon />
-				</IconButton>
-				<Typography variant='h6' className={classes.title}>React Logbook</Typography>
-				{auth ? (
-					<div>
+			<AppBar position='static'>
+				<ToolBar variant='dense' className={classes.menuBar}>
+					{!isDesktop ? (
 						<IconButton
-							aria-label="account of user"
-							aria-controls="menu-appbar"
-							onClick={handleMenu}
-							color="inherit"
+							edge='start'
+							className={classes.menuButton}
+							color='inherit'
+							aria-label='menu'
 						>
-							<AccountCircle />
+							<MenuIcon />
 						</IconButton>
-					</div>
-				) : null}
-			</ToolBar>
-		</AppBar>
+					) : null}
+					<Typography variant='h6' className={classes.title}>
+						React Logbook
+					</Typography>
+					{auth ? (
+						<div>
+							<IconButton
+								aria-label='account of user'
+								aria-controls='menu-appbar'
+								onClick={handleMenu}
+								color='inherit'
+							>
+								<AccountCircle />
+							</IconButton>
+						</div>
+					) : (
+						<div>
+							<Button
+								aria-label='login'
+								aria-controls='menu-appbar'
+								onClick={toggle}
+								color='inherit'
+							>
+								Login
+							</Button>
+						</div>
+					)}
+				</ToolBar>
+			</AppBar>
 		</div>
-	)
-}
+	);
+};
 
-export default HeaderNav
+export default HeaderNav;
