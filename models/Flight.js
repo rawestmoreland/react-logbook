2a59d516aa132be9fae30328144b1e6f8ea4366c
Richard Westmoreland <kegtrack@gmail.com>
2020-03-21 07:29:59 -0500

    Fix the router error.
    Was missing a slash before routes. OOps

--- models/Flight.js
+++ models/Flight.js
@@ -1,101 +1,101 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 // Create the schema for a flight
-const FlightSchema = new Schema ({
-    date: {
-        type: Date,
-        required: true
-    },
-    aircraft_id: {
-        type: String,
-        required: true
-    },
-    route: {
-        type: String,
-        required: true
-    },
-    takeoffs: {
-        type: Number,
-        required: true
-    },
-    day_ldgs: {
-        type: Number,
-        default: 0
-    },
-    night_ldgs: {
-        type: Number,
-        default: 0
-    },
-    approaches: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    single_engine: {
-        type: Number,
-        default: 0.0
-    },
-    multi_engine: {
-        type: Number,
-        default: 0.0
-    },
-    complex: {
-        type: Number,
-        default: 0.0
-    },
-    turbine: {
-        type: Number,
-        default: 0
-    },
-    jet: {
-        type: Number,
-        default: 0.0
-    },
-    sim: {
-        type: Number,
-        default: 0.0
-    },
-    xc: {
-        type: Number,
-        default: 0.0
-    },
-    imc: {
-        type: Number,
-        default: 0.0
-    },
-    sim_imc: {
-        type: Number,
-        default: 0.0
-    },
-    total: {
-        type: Number,
-        required: true
-    },
-    night: {
-        type: Number,
-        deafult: 0.0
-    },
-    pic: {
-        type: Number,
-        default: 0.0
-    },
-    sic: {
-        type: Number,
-        default: 0.0
-    },
-    dual_recd: {
-        type: Number,
-        default: 0.0
-    },
-    dual_given: {
-        type: Number,
-        default: 0.0
-    },
-    remarks: {
-        type: String,
-        default: ""
-    }
-})
+const FlightSchema = new Schema({
+	date: {
+		type: Date,
+		required: true
+	},
+	aircraft_id: {
+		type: String,
+		required: true
+	},
+	route: {
+		type: String,
+		required: true
+	},
+	takeoffs: {
+		type: Number,
+		required: true
+	},
+	day_ldgs: {
+		type: Number,
+		default: 0
+	},
+	night_ldgs: {
+		type: Number,
+		default: 0
+	},
+	approaches: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	single_engine: {
+		type: Number,
+		default: 0.0
+	},
+	multi_engine: {
+		type: Number,
+		default: 0.0
+	},
+	complex: {
+		type: Number,
+		default: 0.0
+	},
+	turbine: {
+		type: Number,
+		default: 0
+	},
+	jet: {
+		type: Number,
+		default: 0.0
+	},
+	sim: {
+		type: Number,
+		default: 0.0
+	},
+	xc: {
+		type: Number,
+		default: 0.0
+	},
+	imc: {
+		type: Number,
+		default: 0.0
+	},
+	sim_imc: {
+		type: Number,
+		default: 0.0
+	},
+	total: {
+		type: Number,
+		required: true
+	},
+	night: {
+		type: Number,
+		deafult: 0.0
+	},
+	pic: {
+		type: Number,
+		default: 0.0
+	},
+	sic: {
+		type: Number,
+		default: 0.0
+	},
+	dual_recd: {
+		type: Number,
+		default: 0.0
+	},
+	dual_given: {
+		type: Number,
+		default: 0.0
+	},
+	remarks: {
+		type: String,
+		default: ''
+	}
+});
 
-module.exports = Flight = mongoose.model('flight', FlightSchema)
+module.exports = Flight = mongoose.model('flight', FlightSchema);
--- routes/aircraft.js
+++ routes/aircraft.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {getAircraft, addAircraft, deleteAircraft} = require('../controllers/aircraft');
+const {
+	getAircraft,
+	addAircraft,
+	deleteAircraft
+} = require('../controllers/aircraft');
 
-router
-	.route('/')
-	.get(getAircraft);
+router.route('/').get(getAircraft);
 
-router
-	.route('/')
-	.post(addAircraft);
+router.route('/').post(addAircraft);
 
-router
-	.route('/:id')
-	.delete(deleteAircraft);
+router.route('/:id').delete(deleteAircraft);
 
 module.exports = router;
