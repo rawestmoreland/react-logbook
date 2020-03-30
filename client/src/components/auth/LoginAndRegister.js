539de89ccf6bfe546b5626c5c28a06c21e08c9cb
Richard Westmoreland <rawestmoreland@gmail.com>
2020-03-29 21:04:34 -0500

    Start the login and register component. Beging to set up Redux.
    In the state there will be flights, aircraft and auth state.

---
+++ client/src/components/auth/LoginAndRegister.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const LoginAndRegister = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LoginAndRegister;
