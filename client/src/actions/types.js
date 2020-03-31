b2c6af3b17a877f17877303c93163dbc5f323ae2
Richard Westmoreland <kegtrack@gmail.com>
2020-03-30 21:08:43 -0500

    start action types

---
+++ client/src/actions/types.js
@@ -0,0 +1,12 @@
+export const GET_FLIGHTS = 'GET_FLIGHTS';
+export const ADD_FLIGHTS = 'ADD_FLIGHT';
+export const DELETE_FLIGHT = 'DELETE_FLIGHT';
+export const FLIGHTS_LOADING = 'FLIGHTS_LOADING';
+export const AIRCRAFT_LOADING = 'AIRCRAFT_LOADING';
+export const GET_AIRCRAFT = 'GET_AIRCRAFT';
+export const ADD_AIRCRAFT = 'ADD_AIRCRAFT';
+export const DELETE_AIRCRAFT = 'DELETE_AIRCRAFT';
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "user_LOADED";
+export const LOGIN_FAIL - 'LOGIN_FAIL';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
